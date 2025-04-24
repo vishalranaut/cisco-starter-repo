9f258594600964afea397a013c13dc94b6c4d43c
Vishal Ranaut <vishalkumar17153@gmail.com>
2025-04-24 19:12:38 +0530

    Add additional Exhibit components for system info and weather forecast

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -7,7 +7,12 @@ function App() {
   return (
     <div>
       <Banner />
-
+      <Exhibit title="System Info">
+        <p>This could be your OS or browser details.</p>
+      </Exhibit>
+      <Exhibit title="Weather Forecast">
+        <p>Some weather component could go here.</p>
+      </Exhibit>
       <Exhibit title="Public IP Address">
         <PublicIP />
       </Exhibit>
